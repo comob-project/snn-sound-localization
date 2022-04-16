54a1704141bd498c8b56fb7e1414e64c1527419d
Tomas Fiers <tomas.fiers@gmail.com>
2022-04-16 13:27:15 +0100

    web: change author ("COMOB: the project for…"); code comments

--- web/build.py
+++ web/build.py
@@ -1,15 +1,15 @@
 # Script that
-# 1) modifies the config and table of contents for the website; and
+# 1) modifies the config and table of contents (toc) for the website; and
 # 2) builds the website (i.e. generate html) using jupyterbook (`jb`).
 #
 # To be run from the repo root (i.e. the same directory that contains `notebooks/`).
 
 
-from util import read, write, run
+from util import read, write, run  # local helper functions
 
-from os import environ
 from pathlib import Path
 from shutil import rmtree
+from os import environ
 
 
 
@@ -42,8 +42,7 @@ def to_toc_entry(p: Path):
 
 toc["chapters"] = [to_toc_entry(nbpath) for nbpath in nbdir.glob("*.ipynb")]
 
-tocfile = outdir / "_toc__completed.yml"
-write(toc, tocfile)
+write(toc, tocfile := outdir / "_toc__completed.yml")
 
 
 
@@ -54,12 +53,12 @@ write(toc, tocfile)
 config = read(webdir / "_config.yml")
 
 config["repository"]["branch"] = environ.get("GITHUB_REF_NAME", "unknown-branch")
-# This setting is used to create links to source. If we're not running on Githhub
-# Actions (i.e. testing locally), we don't bother getting the branch name.
-# Env var from https://docs.github.com/en/actions/learn-github-actions/environment-variables
+# If we're not running on Githhub Actions (i.e. testing locally), we don't bother
+# getting the current branch name [we could, by calling `git`].
+# Env var from
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables
 
-configfile = outdir / "_config__completed.yml"
-write(config, configfile)
+write(config, configfile := outdir / "_config__completed.yml")
 
 
 
